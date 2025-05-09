a41aa67d01fd0d2cd2f42d7577b7fcb965537b5e
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-05-09 13:55:53 -0600

    Add files via upload

---
+++ Section_2/WholeNumbers.java
@@ -0,0 +1,10 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        long globalPopulation = 8000000000L;
+        long dailyGoogleSearches = 8500000000L;
+
+
+        System.out.println("The global population is " + globalPopulation);
+        System.out.println("Daily there are " + dailyGoogleSearches + " Google searches");
+    }
+}
