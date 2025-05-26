6c80a19b9642b55caaec515b0f8048746e0f82d8
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-05-25 18:05:54 -0600

    Add files via upload

--- Section_7/Main.java
+++ Section_7/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car("Nissan", 10000, 2020, "Green");
+        Car nissan = new Car("Nissan", 10000, 2020, "Green", new String[] {"Tires", "Keys"});
         Car nissan2 = new Car(nissan);
-        nissan2.setColor("Yellow");
-        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue", new String[] {"Tires", "Keys"});
 
-        nissan.setColor("Purple");
-
-        nissan2.drive();
+        System.out.println(nissan);
+        System.out.println(dodge);
+        System.out.println(nissan2);
     }
 }
