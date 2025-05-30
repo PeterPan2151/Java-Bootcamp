390e26ba3b9d448bd934a7a52abe990378e62647
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-05-29 21:20:20 -0600

    Add files via upload

--- Section_7/Main.java
+++ Section_7/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Car[] cars = new Car[] {
-            new Car("Nissan", 5000, 2020, "red", new String[] {"tires", "keys"}),
-            new Car("Dodge", 8500, 2019, "blue", new String[] {"tires", "keys"}),
-            new Car("Nissan", 5000, 2017, "yellow", new String[] {"tires", "filter"}),
-            new Car("Honda", 7000, 2019, "orange", new String[] {"tires", "filter"}),
-            new Car("Mercedes", 12000, 2015, "jet black", new String[] {"tires", "filter", "transmission"})
+                new Car("Nissan", 5000, 2020, "red", new String[] { "tires", "keys" }),
+                new Car("Dodge", 8500, 2019, "blue", new String[] { "tires", "keys" }),
+                new Car("Nissan", 5000, 2017, "yellow", new String[] { "tires", "filter" }),
+                new Car("Honda", 7000, 2019, "orange", new String[] { "tires", "filter" }),
+                new Car("Mercedes", 12000, 2015, "jet black", new String[] { "tires", "filter", "transmission" })
         };
 
         Dealership dealership = new Dealership(cars);
@@ -22,7 +22,7 @@ public class Main {
         System.out.println("Dealership car make: " + dealership.getCar(0).getMake());
 
         // Create a new Car object
-        Car newCar = new Car("BMW", 10000, 2018, "silver", new String[] {"tires", "filter", "transmission"});
+        Car newCar = new Car("BMW", 10000, 2018, "silver", new String[] { "tires", "filter", "transmission" });
 
         // Set the car at index 1 in the dealership to a deep copy of the newCar
         dealership.setCar(1, newCar);
