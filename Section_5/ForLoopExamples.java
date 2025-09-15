1cf8dc2355e3621243c343691d033ae004702491
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-09-14 20:34:23 -0600

    Add files via upload

--- Section_5/ForLoopExamples.java
+++ Section_5/ForLoopExamples.java
@@ -1,24 +1,28 @@
 public class ForLoopExamples {
 
     public static void main(String[] args) {
-    
+        // TODO: Write a for loop that prints numbers from 1 to 10
         for (int i = 1; i < 11; i++) {
             System.out.println(i);
         }
 
-        for(int i = 0; i < 5; i++) {
-            System.out.println("Hello World");
+        // TODO: Write a for loop that prints "Hello, world!" 5 times
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello, world!");
         }
 
-        for (int i = 1; i < 11; i++) {
+        // TODO: Write a for loop that prints the multiplication table of 5 up to 10 (5
+        for (int i = 5; i < 11; i++) {
             System.out.println("5 x " + i + " = " + (i * 5));
         }
 
+        // TODO: Write a for loop that prints numbers from 10 to 1 in reverse order
         for (int i = 10; i > 0; i--) {
             System.out.println(i);
         }
 
-        for (int i = 1; i < 11; i++) {
+        // TODO: Write a for loop that prints the first 10 even numbers
+        for (int i = 1; i < 21; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
