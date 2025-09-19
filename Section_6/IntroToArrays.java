0c07751fe225c71b559780075568ac76e72db9f8
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-09-18 20:48:29 -0600

    Add files via upload

--- Section_6/IntroToArrays.java
+++ Section_6/IntroToArrays.java
@@ -1,6 +1,6 @@
 public class IntroToArrays {
     public static void main(String[] args) {
-        String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "East Anglia"};
+        String[] kingdoms = { "Mercia", "Wessex", "Northumbria", "East Anglia" };
         System.out.println(kingdoms[0]);
         System.out.println(kingdoms[1]);
         System.out.println(kingdoms[2]);
